b3482398845e0a7b32d70bd580d53bb56892a239
thirumagalmeena <thirumagalmeena29@gmail.com>
2025-07-23 17:54:55 +0530

    Deleted files

--- backend/config.py
+++
@@ -1,6 +0,0 @@
-MYSQL_HOST = 'localhost'
-MYSQL_PORT = 3306  # Add this line
-MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'Sanju@123'
-MYSQL_DB = 'EquipmentMaintenanceNew'
-MYSQL_CURSORCLASS = 'DictCursor'
